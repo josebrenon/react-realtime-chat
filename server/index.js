c4270d70f139302b2ea04ccfcfb4427b961c15a1
José Brenon <eibrenon88@gmail.com>
2024-11-27 17:06:29 -0300

    proximo teste 001

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
-  cors: { origin: "https://josebrenon-reactrealtimechat.netlify.app/" },
+  cors: {
+    origin: "https://josebrenon-reactrealtimechat.netlify.app/",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
 });
 
 const PORT = 404;

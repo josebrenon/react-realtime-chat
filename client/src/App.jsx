819b55111690a860abb8f2c68e9e49839ce69261
José Brenon <eibrenon88@gmail.com>
2024-11-27 15:46:41 -0300

    Add o repo

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import Chat from "./components/Chat/Chat";
+import Join from "./components/Join/Join";
+
+function App() {
+  const [chatVisibility, setChatVisibility] = useState(false);
+  const [socket, setSocket] = useState(null);
+
+  return (
+    <div className="App">
+      {chatVisibility ? (
+        <Chat socket={socket} />
+      ) : (
+        <Join setSocket={setSocket} setChatVisibility={setChatVisibility} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Join/Join.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React, { useRef, useEffect } from "react";
+import io from "socket.io-client";
+import style from "./Join.module.css";
+import { Input, Button } from "@mui/material";
+
+export default function Join({ setChatVisibility, setSocket }) {
+  const usernameRef = useRef();
+
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
+
+  const handleSubmit = async () => {
+    const username = usernameRef.current.value;
+    if (!username.trim()) return;
+    const socket = await io.connect("http://localhost:3001");
+    socket.emit("set_username", username);
+    setSocket(socket);
+    setChatVisibility(true);
+  };
+  const getEnterKey = (e) => {
+    if (e.key === "Enter") handleSubmit();
+  };
+
+  return (
+    <div className={style["join-container"]}>
+      <h2>Chat em tempo real</h2>
+      <Input
+        inputRef={usernameRef}
+        placeholder="Nome de usuário"
+        onKeyDown={(e) => getEnterKey(e)}
+      />
+      <Button sx={{ mt: 2 }} onClick={() => handleSubmit()} variant="contained">
+        Entrar
+      </Button>
+    </div>
+  );
+}

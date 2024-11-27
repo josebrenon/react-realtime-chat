665d328251b2aab7fde34563e2c10307e42f9d9a
José Brenon <eibrenon88@gmail.com>
2024-11-27 15:51:00 -0300

    trocando a origem

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "https://josebrenon-reactrealtimechat.netlify.app/" },
 });
 
 const PORT = 3001;
